97f39614f6cb23521fa0483d39ced8f7f2a27e51
shakilkarim <rezaul921gcr@gmail.com>
2023-03-25 10:49:03 +0600

    Change file

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, login, profileUpdate } = require('../controllers/auth');
-const { requireSignin, isAdmin } = require('../middlewares/auth');
-
-const router = express.Router();
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
